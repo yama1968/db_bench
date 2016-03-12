db507a87625d2f4f7c0ee40cf82cc5ffcb4bd529
yama1968 <ymartel@gmail.com>
2016-03-12 12:02:08 +0100

    more test with patty direct

--- monetdb.update02.sql
+++ monetdb.update02.sql
@@ -1,5 +1,7 @@
 
-drop table device_id_nb_tmp;
+start transaction;
+
+#drop table device_id_nb_tmp;
 
 create table device_id_nb_tmp
        as select * from 
@@ -9,8 +11,13 @@ create table device_id_nb_tmp
        where foo.nnb > 1
        with data;
 
-alter table train 
-      drop column nnb;
+#create index device_id_nb_tmp1 on device_id_nb_temp(device_id);
+
+#drop index train1;
+#create index train1 on train(device_id);
+
+#alter table train 
+#      drop column nnb;
 
 alter table train
       add nnb integer;
@@ -20,7 +27,7 @@ update train
                              where train.device_id = dt.device_id);
 
 select annb, count(*)
-       from (select device_id, avg(nnb) as annb
+       from (select device_id, max(nnb) as annb
                     from train
                     group by device_id) as foo
        group by annb
@@ -28,6 +35,9 @@ select annb, count(*)
 
 drop table device_id_nb_tmp;
 
+#drop index train1;
+
 alter table train 
       drop column nnb;
 
+commit;
