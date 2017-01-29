75cd57662071ca8305555bf3fbe2566455894bfa
Yannick Martel <ymartel@gmail.com>
2017-01-29 21:59:45 +0100

    added impala.do.sh, more generic launch, plus impala.log01.2017-01-29-20:54:40.log after stats

---
+++ impala.compute.stats.sql
@@ -0,0 +1,16 @@
+--
+
+select count(*) from train;
+select * from train limit 2;
+
+show table stats train;
+
+show column stats train;
+
+compute stats train;
+
+show table stats train;
+
+select count(*) from train;
+select * from train limit 2;
+
